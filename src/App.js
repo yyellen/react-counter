d503fbf2149f92c235fbeb277a38d25a35faf908
yyellen <yyopenfolder@gmail.com>
2022-12-30 17:25:48 +0800

    make tailwind as module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import MyButton from "./MyButton";
 
 function App() {
   return (
-    <div className="container">
-      <div className='text-sky-400 text-[64px] hover:text-pink-500'>Hello React</div>
+    <div className="container w-screen h-screen mx-auto bg-gray-100 flex justify-center items-center">
+      <MyButton />
     </div>
   );
 }
---
+++ src/MyButton/index.js
@@ -0,0 +1,9 @@
+import style from './MyButton.module.css'
+
+const MyButton = () => { 
+  return (
+    <div className={style.my_button}>Click Me</div>
+  )
+};
+
+export default MyButton;
