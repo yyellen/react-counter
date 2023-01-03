ca7b57271409db7bc2917990c75afa6fa9b103fd
yyellen <yyopenfolder@gmail.com>
2023-01-03 15:13:43 +0800

    add useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-import MyButton from "./MyButton";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const add = () => {
+    setNumber(number + 1);
+  };
+
+  const sub = () => {
+    setNumber(number - 1);
+  };
+
   return (
-    <div className="container w-screen h-screen mx-auto bg-gray-100 flex justify-center items-center">
-      <MyButton />
+    <div className="container w-screen h-screen mx-auto bg-gray-100 flex justify-center items-center flex-col">
+      <div className="counter">
+        <button onClick={sub}>-</button>
+        <p className="number">{number}</p>
+        <button onClick={add}>+</button>
+      </div>
+      <div>{number}</div>
     </div>
   );
 }
