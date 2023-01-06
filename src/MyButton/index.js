353fd03c1767a93637364f6b2bb3e1804e88e32c
yyellen <yyopenfolder@gmail.com>
2023-01-06 15:36:53 +0800

    remove MyButton

--- src/MyButton/index.js
+++
@@ -1,9 +0,0 @@
-import style from './MyButton.module.css'
-
-const MyButton = ({text}, {effect}) => { 
-  return (
-    <div className={style.my_button} onClick={effect}>{text}</div>
-  )
-};
-
-export default MyButton;
