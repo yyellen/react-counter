d1fdae61189ba5aa92a391f498775185d645472f
yyellen <yyopenfolder@gmail.com>
2023-01-06 16:01:31 +0800

    feat: add util for prime check

---
+++ src/utils/util.js
@@ -0,0 +1,13 @@
+export const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  
+  return true;
+};
